cbae52de82a53f98ee30733addec002219940e80
italoplk <idmachado@inf.ufpel.edu.br>
2024-03-04 14:28:41 +0100

    chng loss dflt now is SATD

--- src/Params.py
+++ src/Params.py
@@ -30,7 +30,7 @@ def get_args():
     parser.add_argument('--epochs', type=int, default=100, help='Epochs to test (default: 100)')
     parser.add_argument('--batch-size', type=int, default=64,
                         help='Batch size (default: 64). For crop dataloaders, teh actual BS is multiplied by crops_per_image')
-    parser.add_argument('--loss', type=str, default='mse', help='Loss functionto minimize [mse|satd]')
+    parser.add_argument('--loss', type=str, default='satd', help='Loss functionto minimize [mse|satd]')
     parser.add_argument('--loss-mode', type=str, default='predOnly', help='Defines context for loss [predOnly|fullContext]')
     #parser.add_argument('--context-mode', type=str, default='black', help='Defines context for prediction [black|average]')
 
